242319be6ce1e9b74bd6b2fc05712fc771e87171
luoding1 <luoding@qq.com>
2020-06-16 11:02:49 +0800

    aaa

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tgwstyle.github.io
+
+js框架：vue.js \
+css样式框架：element
---
+++ html/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="web">
+        {{message}}
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+    export default {
+        name:'index',
+        data(){
+            message:'你好'
+        }
+    }
+
+</script>
